b0cb5d6cd460b4ba3f45639020a528f70c2e2343
alpeshmail@gmail.com <alpeshmail@gmail.com>
2015-12-22 22:33:43 -0500

    add inilize project

---
+++ DashboardTestApi/Controllers/SecController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DashboardTestApi.Controllers
+{
+    [RoutePrefix("api/sec")]
+    public class SecController : ApiController
+    {
+        [HttpGet]
+        [Route("data")]
+        public string GetData()
+        {
+            return "test data";
+        }
+    }
+}
---
+++ DashboardTestApi/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DashboardTestApi.Controllers
+{
+    [RoutePrefix("test")]
+    public class TestController : ApiController
+    {
+        // GET: api/test
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Test
+        [Route("orders")]
+        [HttpGet]
+        public IEnumerable<string> orders()
+        {
+            return new string[] { "value3", "value2" };
+        }
+
+
+    }
+}
